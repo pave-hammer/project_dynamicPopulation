fa816a3596d8715336a5e5795d8ff7209e78f3a8
Mike <szekleym@gmail.com>
2018-11-09 10:58:46 -0700

    added bones to index.html, added basic css and created array for tiles

---
+++ list.js
@@ -0,0 +1,12 @@
+var familyGuy = [
+    {name: 'Peter Griffin', age: 1, job: '', image: ''},
+    {name: 'Lois Griffin', age: 1, job: '', image: ''},
+    {name: 'Chris Griffin', age: 1, job: '', image: ''},
+    {name: 'Stewie Griffin', age: 1, job: '', image: ''},
+    {name: 'Brian Griffin', age: 1, job: '', image: ''},
+    {name: 'Glen Quagmire', age: 1, job: '', image: ''},
+    {name: 'Joe Swanson', age: 1, job: '', image: ''},
+    {name: 'Cleveland Brown', age: 1, job: '', image: ''},
+    {name: 'Mort Goldman', age: 1, job: '', image: ''},
+    {name: 'Meg (no one cares)', age: 1, job: '', image: ''}
+]
